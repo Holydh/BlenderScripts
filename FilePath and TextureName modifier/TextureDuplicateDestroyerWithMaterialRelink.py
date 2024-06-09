8317a65d97015235ca7dd4e034a5c24f8b68cfae
Holydh <109091343+Holydh@users.noreply.github.com>
2024-06-09 02:16:04 +0200

    Update TextureDuplicateDestroyerWithMaterialRelink.py

--- FilePath and TextureName modifier/TextureDuplicateDestroyerWithMaterialRelink.py
+++ FilePath and TextureName modifier/TextureDuplicateDestroyerWithMaterialRelink.py
@@ -1,21 +1,33 @@
-#Blender 4.1
+# Using Blender 4.1
+
 # Use DupeGuru to generate the duplicate report .csv file : https://github.com/arsenetar/dupeguru/
 # DupeGuru scans folders for duplicate files by analysing and comparing the content of each.
 
 # This script will then use this csv file to relink the materials using the duplicates to the original then will delete the .png files from the folder. 
 # It need to be used with the textures unpacked and renamed, textures must have the same name has their materials (but still with the .png extension of course)
-# So here's the steps summarized :
+
+# So here's the required steps summarized :
 # 1 - BatchRenameBlenderImageNameFromMaterialName.py (run the script from the blender text editor)
 # 2 - RenamePackedTextureFilePathFromBlenderImageName.py (In the blender python console)
 # 3 - Unpack textures in current directory
 # 4 - Run this script below
+
+# /!\ Depending on your OS language, you'll need to change the read_csv function with the proper csv rows. Open your csv and look up the first line.
+# This is how the csv rows looks in french :
+# Group ID,Nom de fichier,Dossier,Taille (KB),Dimensions,Match %
+
+# Find these following lines (lines 37, 38, 39) in the script and replace the csv rows with yours. eg : 'Nom de fichier' should be something like 'folder name' etc.
+#            group_id = row['Group ID']
+#            file_name = row['Nom de fichier']
+#            folder = row['Dossier']
+
 
 import bpy
 import csv
 import os
 
 # Path to the CSV file
-csv_path = "C:\\Users\\pathToYourDuplicateReport.csv"
+csv_path = "C:\\Users\\PathToYourDuplicateReport.csv"
 
 def read_csv(csv_path):
     groups = {}
@@ -30,7 +42,7 @@ def read_csv(csv_path):
             groups[group_id].append((file_name, folder))
     return groups
 
-def relink_duplicate_textures_and_delete_files(groups):
+def relink_and_delete_duplicates(groups):
     for group_id, files in groups.items():
         if not files:
             continue
@@ -38,28 +50,46 @@ def relink_duplicate_textures_and_delete_files(groups):
         # Determine the main file for this group
         main_file = files[0]
         main_texture_name = os.path.splitext(main_file[0])[0] + '.png'
+        main_material_name = os.path.splitext(main_file[0])[0]
 
-        # Get the main image in Blender
+        # Get the main image and material in Blender
         main_image = bpy.data.images.get(main_texture_name)
-        if not main_image:
-            print(f"Main image {main_texture_name} not found in Blender.")
+        main_material = bpy.data.materials.get(main_material_name)
+        if not main_image or not main_material:
+            print(f"Main image or material {main_texture_name}/{main_material_name} not found in Blender.")
             continue
 
-        # Relink all other textures in the group to the main texture and delete them
+        # Relink all other materials in the group to the main material
         for file_name, folder in files[1:]:
             texture_name = os.path.splitext(file_name)[0] + '.png'
             image = bpy.data.images.get(texture_name)
+            material_name = os.path.splitext(file_name)[0]
+            material = bpy.data.materials.get(material_name)
+
             if image:
-                for material in bpy.data.materials:
-                    if material.use_nodes:
-                        for node in material.node_tree.nodes:
+                for mat in bpy.data.materials:
+                    if mat.use_nodes:
+                        for node in mat.node_tree.nodes:
                             if node.type == 'TEX_IMAGE' and node.image == image:
                                 node.image = main_image
                                 print(f"Relinked {texture_name} to {main_texture_name}")
                 
                 # Remove the image data-block from Blender
                 bpy.data.images.remove(image)
-            
+
+            if material:
+                # Relink objects using the duplicate material to the main material
+                for obj in bpy.data.objects:
+                    if obj.type == 'MESH':
+                        for slot in obj.material_slots:
+                            if slot.material == material:
+                                slot.material = main_material
+                                print(f"Relinked material {material_name} to {main_material_name}")
+
+                # Remove the material data-block from Blender
+                bpy.data.materials.remove(material)
+                print(f"Deleted material {material_name}")
+
             # Construct the full path of the image file and delete it
             file_path = os.path.join(folder, file_name)
             try:
@@ -70,7 +100,7 @@ def relink_duplicate_textures_and_delete_files(groups):
 
 def main():
     groups = read_csv(csv_path)
-    relink_duplicate_textures_and_delete_files(groups)
+    relink_and_delete_duplicates(groups)
 
 if __name__ == "__main__":
     main()
