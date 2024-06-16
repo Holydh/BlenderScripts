c5cde75b087bac7d413cdf5a7b384cd1a4658ffe
Holydh <109091343+Holydh@users.noreply.github.com>
2024-06-16 16:14:02 +0200

    Update Link BSDF Principled Nodes to Material Output Surface on all materials.py

--- Material modifiers/Link BSDF Principled Nodes to Material Output Surface on all materials.py
+++ Material modifiers/Link BSDF Principled Nodes to Material Output Surface on all materials.py
@@ -1,13 +1,20 @@
 import bpy
 
 for eachMat in bpy.data.materials:
-    nodes = eachMat.node_tree.nodes
-    links = eachMat.node_tree.links
-    principled_bsdf = nodes.get("Principled BSDF")
-    material_output = nodes.get("Material Output")
-    for n in nodes:
-        if (n.type == 'BSDF_PRINCIPLED'):
-            BSDFcheck = n.outputs[0]
-            if not(BSDFcheck.is_linked):
-                # Connect the Principled BSDF node to the Material Output node
-                links.new(material_output.inputs[0], principled_bsdf.outputs[0])
+    if eachMat.use_nodes:  # Check if the material uses nodes
+        nodes = eachMat.node_tree.nodes
+        links = eachMat.node_tree.links
+        principled_bsdf = None
+        material_output = None
+        
+        # Find Principled BSDF and Material Output nodes
+        for node in nodes:
+            if node.type == 'BSDF_PRINCIPLED':
+                principled_bsdf = node
+            elif node.type == 'OUTPUT_MATERIAL':
+                material_output = node
+
+        # Ensure both nodes are found
+        if principled_bsdf and material_output:
+            # Connect the Principled BSDF node to the Material Output node
+            links.new(principled_bsdf.outputs[0], material_output.inputs[0])
